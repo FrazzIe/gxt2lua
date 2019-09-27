61b92dfc298f40516eb480c15cd2565720538f87
Fraser <28698343+FrazzIe@users.noreply.github.com>
2019-09-27 23:53:00 +0100

    Add src files

---
+++ gxt2lua/Main.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace gxt2lua
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputText = new System.Windows.Forms.TextBox();
+            this.outputText = new System.Windows.Forms.TextBox();
+            this.inputLabel = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.creditLabel = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // inputText
+            // 
+            this.inputText.Location = new System.Drawing.Point(13, 43);
+            this.inputText.Multiline = true;
+            this.inputText.Name = "inputText";
+            this.inputText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.inputText.Size = new System.Drawing.Size(306, 342);
+            this.inputText.TabIndex = 0;
+            this.inputText.TextChanged += new System.EventHandler(this.InputText_TextChanged);
+            // 
+            // outputText
+            // 
+            this.outputText.Location = new System.Drawing.Point(331, 43);
+            this.outputText.Multiline = true;
+            this.outputText.Name = "outputText";
+            this.outputText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputText.Size = new System.Drawing.Size(306, 342);
+            this.outputText.TabIndex = 1;
+            // 
+            // inputLabel
+            // 
+            this.inputLabel.AutoSize = true;
+            this.inputLabel.Location = new System.Drawing.Point(13, 24);
+            this.inputLabel.Name = "inputLabel";
+            this.inputLabel.Size = new System.Drawing.Size(31, 13);
+            this.inputLabel.TabIndex = 2;
+            this.inputLabel.Text = "Input";
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(331, 24);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(39, 13);
+            this.outputLabel.TabIndex = 3;
+            this.outputLabel.Text = "Output";
+            // 
+            // creditLabel
+            // 
+            this.creditLabel.AutoSize = true;
+            this.creditLabel.Location = new System.Drawing.Point(540, 396);
+            this.creditLabel.Name = "creditLabel";
+            this.creditLabel.Size = new System.Drawing.Size(97, 13);
+            this.creditLabel.TabIndex = 4;
+            this.creditLabel.TabStop = true;
+            this.creditLabel.Text = "Created by Frazzle.";
+            this.creditLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.CreditLabel_LinkClicked);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 418);
+            this.Controls.Add(this.creditLabel);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.inputLabel);
+            this.Controls.Add(this.outputText);
+            this.Controls.Add(this.inputText);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "gxt2lua";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputText;
+        private System.Windows.Forms.TextBox outputText;
+        private System.Windows.Forms.Label inputLabel;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.LinkLabel creditLabel;
+    }
+}
+
---
+++ gxt2lua/Main.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+namespace gxt2lua
+{
+    public partial class frmMain : Form
+    {
+        //Declare global vars
+        Regex regex;
+        string github = "https://github.com/FrazzIe";
+
+        public frmMain()
+        {
+            InitializeComponent();
+            regex = new Regex(@"(0[xX][0-9a-fA-F]+)(\s=\s)(\w+.*)", RegexOptions.Multiline); //Init regex obj
+        }
+
+        private void InputText_TextChanged(object sender, EventArgs e) //Event triggered when the input text is changed
+        {
+            string output = ""; //new output
+            int count = 0; //counter
+            MatchCollection matches = regex.Matches(inputText.Text); //collection of regex matchs
+
+            if (matches.Count > 0) //Check if theres any matches
+            {
+                foreach (Match match in matches) //Loop through each match
+                {
+                    GroupCollection groups = match.Groups; //collection of groups in a regex match
+
+                    if (groups.Count >= 4) //check if there are enough groups in a match to continue
+                    {
+                        if (count > 0) //Check if this isn't the first match
+                        {
+                            output += "\r\n"; //Add new line
+                        }
+
+                        var hash = groups[1].Value; //Text Entry Hash
+                        var value = Regex.Replace(groups[3].Value, @"\t|\n|\r", ""); //Text Entry Value
+
+                        output += "AddTextEntryByHash(" + hash + ", \"" + value + "\")\r\n"; //Add match to output
+
+                        count++; //increment match counter
+                    }
+                }
+            }
+
+            if (string.IsNullOrEmpty(output)) //Check if input is empty
+            {
+                outputText.Text = "No matches found?"; //Send error msg
+            } else
+            {
+                outputText.Text = output; //Send formatted list of matches
+            }
+            
+        }
+
+        private void CreditLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) //Event triggered on linked label click
+        {
+            Process.Start(github); //Open link to github
+        }
+    }
+}
